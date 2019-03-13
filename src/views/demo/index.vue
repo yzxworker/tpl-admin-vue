33d581a4a17fda8e24f83efb1f86ae4a2f1a8c6d
liu.hl <liuhonglin@xsyxsc.com>
2019-03-14 01:57:33 +0800

    添加组件之间的数据流

--- src/views/demo/index.vue
+++ src/views/demo/index.vue
@@ -1,27 +1,45 @@
 <template>
   <div class="demo-container">
     <h1>demo</h1>
+    <!--组件之间的数据流-->
+    <product-item
+      :title="productTitle"
+      :price="productPrice"
+      @outPrice="onProductItemPrice"
+    ></product-item>
+    <p>父组件中的价格：{{productPrice}}</p>
+    <button @click="onChangePrice">父组件事件</button>
   </div>
 </template>
 
 <script>
-
+import ProductItem from './product-item'
 export default {
   name: 'Demo',
   components: {
+    ProductItem
   },
   filters: {
 
   },
   data() {
-    return {}
+    return {
+      productTitle:'水果',
+      productPrice:10
+    }
   },
   computed: {},
   created() {
 
   },
   methods: {
-
+    onChangePrice(){
+      this.productPrice = 100;
+    },
+    onProductItemPrice(data){
+      console.log('======onProductItemPrice=====',data);
+      this.productPrice = data
+    }
   }
 }
 </script>
---
+++ src/views/demo/product-item.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="product-item-container">
+        <h1>{{title}}</h1>
+        <p>子组件data中的价格&yen;{{currentPrice}}</p>
+        <p>子组件props中的价格&yen;{{price}}</p>
+        <button @click="onChangePrice">子组件改变价格</button>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'product-item',
+        props:{
+          title:{
+            type:String,
+            default:'商品标题'
+          },
+          price:{
+            type:Number,
+            default: 0
+          }
+        },
+        components: {},
+        data() {
+            return {
+              currentPrice:this.price
+            }
+        },
+        computed: {
+
+        },
+        created() {
+        },
+        mounted(){
+          console.log('==product-item mounted==');
+        },
+        updated(){
+          console.log('==product-item updated==',this);
+        },
+        methods: {
+          onChangePrice(){
+            this.currentPrice +=10;
+            this.$emit('outPrice',this.currentPrice)
+          }
+        }
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+    .product-item {
+        &-container {
+            margin: 30px;
+        }
+    }
+</style>
